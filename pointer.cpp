31343bdd26298f99411fa0402d6a78a68e952897
Sohanur Rahman <113714857+SoRaS20@users.noreply.github.com>
2024-05-15 20:29:57 +0600

    Create pointer.cpp

---
+++ pointer.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int32_t main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  int num = 20;
+  int *point_variable;
+  point_variable = &num;
+  cout<<*point_variable<<endl;
+  return 0;
+}
